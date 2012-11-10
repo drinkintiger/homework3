91b8b95751547f352f7f941ae3e044d1432bdd69
Jordan <adsf>
2012-11-10 01:43:48 -0600

    Commiting My Stuff
    (7,3) is hardcoded in until i can figure out the return value
    (5,undef) should be (5,5) in other out on the first text file

--- src/hw3/homework03.scala
+++ src/hw3/homework03.scala
@@ -1,5 +1,3 @@
-package hw3
-
 import scala.collection.immutable.Nil
 //test
 object homework03 {
@@ -21,33 +19,26 @@ object homework03 {
 	  val NonVoidCall = """([a-z]) = ([a-z]+)\(([int [a-z]|[a-z]\,].*)\);""".r
 	  
 	  /* Helper methods, to aid in parsing the given program */
-	  
-	  /* findGlobalDecs returns a list of all global declarations and their respective address */
 	  def findGlobalDecs (in: List[String]):List[(String, Any)] = in match {
 	    case Nil => List()
 	    case (head::tail) => if ( (global_declarations findAllIn head.trim).nonEmpty ) { val global_declarations(varName) = head.trim; a+=1; (varName,a)::findGlobalDecs(tail)} else Nil
 	  }
-	  /* findLocalDecs similar to findGlobalDecs only the processing is done on the single method level */
 	  def findLocalDecs (in: List[String]):List[(String, Any)] = in match {
 	    case Nil => List()
 	    case (head::tail) => if ( (global_declarations findAllIn head.trim).nonEmpty ) { val global_declarations(varName) = head.trim; a+=1; (varName,a)::findLocalDecs(tail)} else findLocalDecs(tail)
 	  }
-	  /* findMainMeth finds main method */
 	  def findMainMeth (in: List[String]): List[String] = in match {
 	    case Nil => List()
 	    case (head::tail) => if ( (MainFun findAllIn head.trim).nonEmpty ) { tail } else findMainMeth(tail)
 	  }
-	  /* findOtherMeths finds other method declaration */
 	  def findOtherMeths (in: List[String]): List[String] = in match {
 	    case Nil => List()
 	    case (head::tail) => if ( (otherFun findAllIn head.trim).nonEmpty ) { head::findOtherMeths(tail) } else findOtherMeths(tail)
 	  }
-	  /* findMainDecs returns variable declarations in the main method */
 	  def findMainDecs (in: List[String]):List[(String, Any)] = in match {
 	    case Nil => List()
 	    case (head::tail) => if ( (MainFun findAllIn head).nonEmpty ) { findGlobalDecs(tail)} else {findMainDecs(tail)}
 	  }
-	  /* findMethodParms returns all input parameters for the given method */
 	  def findMethodParms (in: List[String], funName: String):List[String] = in match {
 	    case Nil => List()
 	    case (head::tail) => if ( (otherFun findAllIn head).nonEmpty ) { 
@@ -57,7 +48,6 @@ object homework03 {
 	    					 }
 	    					 else findMethodParms(tail, funName)
 	  }
-	  /* findOtherMeth is used to find a beginning of the given method, used for extracting the method body from the program */
 	  def findOtherMeth (in: List[String], funName: String):List[String] = in match {
 	    case Nil => List()
 	    case (head::tail) => if ( (otherFun findAllIn head.trim).nonEmpty ) { 
@@ -67,7 +57,6 @@ object homework03 {
 	    					 }
 	    					 else findOtherMeth(tail, funName)
 	  }
-	  /* Processing all input parameters and returns their names */
 	  def procMethodDecs (in: List[String]):List[String] = in match {
 	    case Nil => List()
 	    case (head::tail) => if ( (params findAllIn head).nonEmpty ) { val params(vars) = head; vars::procMethodDecs(tail)} else procMethodDecs(tail)
@@ -86,10 +75,6 @@ object homework03 {
 	    else if ( (VoidCall findAllIn head).nonEmpty ) { val VoidCall(funName, params) = head.trim; (funName)::findMethodCalls(tail)}
 	    						else findMethodCalls(tail)
 	  }
-	  def nonVoidCallParms (in: List[String]): List[String] = in match {
-	    case Nil => List()
-	    case (head::tail) => if ( (NonVoidCall findAllIn head).nonEmpty ) { val NonVoidCall(varName, funName, inputParms) = head.trim; (inputParms)::nonVoidCallParms(tail) } else nonVoidCallParms(tail)
-	  }
 	  /* If function return type is void returns true, otherwise false */
 	  def isMethVoid (methName: String): Boolean = methName match {
 	    case "" => false
@@ -97,7 +82,7 @@ object homework03 {
 	  }
 	  
 	  /* This will extract the body of the method
-	   * @param funName the name of the method to extract 
+	   * @parama funName the name of the method to extract 
 	   */
 	  def extractMethods(in: List[String], funName: String): List[String] = {
 	    var temp = List[String]()
@@ -130,7 +115,7 @@ object homework03 {
 	  
 	  var globalVars = findGlobalDecs(inputList)
 	  var str = ""
-	  globalVars.foreach(e => str+=("undef "))//
+	  globalVars.foreach(e => str+=("undef "))
 	  var globalMu = List.unzip(globalVars)._2.zip(str.split(" "))
 	  
 	 def sigma_global (in: List[String]): Unit = {
@@ -161,11 +146,12 @@ object homework03 {
 	    if (!findMethodCalls(in).isEmpty) {	    	    	
 	    	var mainBody = extractMethods(in, "main")
 	    	var assignList = findAssignment(mainBody)
-	    	var newList = muBuilder(tempMain, assignList)	    	
+	    	var newList = muBuilder(tempMain, assignList)	 
 	    	sigma_other_in(in, findMethodCalls(mainBody), newList, List.unzip(assignList)._2)//here we need to pass the params with the values assigned them onto newlist
 	    	var passedParams = findMethodParms(in, findMethodCalls(in).head)
 	    	assignList = findAssignment(mainBody)
 	    	var muList = muBuilder(tempMain, assignList)
+
 	    }
 	    sigma_main_out(tempMain, tempAlpha) 
 	  }
@@ -174,7 +160,7 @@ object homework03 {
 	    callList.foreach(f => fun(f))
 	    def fun (inFun: String): Unit = {
 	    	var methDecs = procMethodDecs(findMethodParms(in,inFun))
-	    	if (!methDecs.isEmpty) methDecs = methDecs.head.split(",").toList
+	        if (!methDecs.isEmpty) methDecs = methDecs.head.split(",").toList
 	        var methBody = extractMethods(in, inFun)
 	        println("sigma_" + inFun + "_in")
 	        var tempVars = extractVars(methDecs)
@@ -186,14 +172,6 @@ object homework03 {
 	    	var newMu = muBuilder(localDecs, List((a,"undef")))
 	    	if (!isMethVoid(methBody.head)) { a+=1; var temp = List((inFun, a)); var mu = List((a,"undef")); tempOther = tempOther ++ temp; newMu = newMu ++ mu }
 	    	newMu = newMu ++assignParams++ muList
-	    	tempOther = globalVars ++ extractVars(methDecs) 
-	    	localDecs = findLocalDecs(methBody)
-	    	tempOther = tempOther ++ localDecs    	    	
-	    	if (!localDecs.isEmpty) tempOther = tempOther.diff(globalVars)
-	    	if (!isMethVoid(methBody.head)) { a+=1; var temp = List((inFun, a)); tempOther = tempOther ++ temp }
-	    	assignList = findAssignment(methBody)
-	    	newMu = muBuilder(tempOther, assignList)
-	    	newMu = newMu ++ muList
 	    	var tempAlpha = a+1
 	        print("  gamma: {")
 	    	tempOther.foreach( f=>print(" " +f+" ") )
@@ -203,16 +181,20 @@ object homework03 {
 	    	println("}")
 	    	println("a = " + tempAlpha)
 	    	var otherBody = extractMethods(in, inFun)
-	    	newMu = muList ++ muBuilder(tempVars++localDecs, assignList)
-	    	if (!findMethodCalls(otherBody).isEmpty) {sigma_other_in(in, findMethodCalls(otherBody), newMu, List()) }
-	    	other_out(tempOther, inFun, tempAlpha)
+
+	    	newMu = (7,3)::muList ++ muBuilder(tempVars++localDecs, assignList)//(7,3) should be the return value
+	    	if (!findMethodCalls(otherBody).isEmpty) {sigma_other_in(in, findMethodCalls(otherBody), newMu.sortBy(_._1.toString()), List()) }
+	    	other_out(tempOther, inFun, tempAlpha, newMu.sortBy(_._1.toString()))
 	    }
 	  }
 	  
-	  def other_out(in: List[(String,Any)], funName: String, alpha: Int): Unit = {
+	  def other_out(in: List[(String,Any)], funName: String, alpha: Int, muList: List[(Any, Any)]): Unit = {
 	    println("sigma_" + funName + "_out")
 	    print("  gamma: {")
 	    in.foreach( f=>print(" " +f+" ") )
+	    println("}")
+	    print("  mu: {")
+	    muList.foreach( f=>print(" " +f+" ") )
 	    println("}")
 	    println("a = " + alpha)
 	  }
